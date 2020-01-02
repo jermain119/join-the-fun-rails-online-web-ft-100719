7637b0fc2197e45bfcf7f365cbad35ca88d2e2f6
jermain119 <jermain119@gmail.com>
2020-01-02 12:13:41 -0600

    Done.

--- db/migrate/20140212204456_create_taxis.rb
+++ db/migrate/20140212204456_create_taxis.rb
@@ -2,7 +2,7 @@ class CreateTaxis < ActiveRecord::Migration
   def change
     create_table :taxis do |t|
       t.timestamps null: false
-      add_foreign_key 
+      
     end
   end
 end
--- db/migrate/20140212205242_create_rides.rb
+++ db/migrate/20140212205242_create_rides.rb
@@ -2,6 +2,7 @@ class CreateRides < ActiveRecord::Migration
   def change
     create_table :rides do |t|
       t.timestamps null: false
+      add_foreign_key :taxi_id
     end
   end
 end
